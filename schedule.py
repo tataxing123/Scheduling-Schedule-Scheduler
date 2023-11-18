7be03b35167d9685e919c3637d501be2d6896921
Tracy Xing <tataxing123@gmail.com>
2023-11-18 12:25:30 -0500

    :)

--- schedule.py
+++ schedule.py
@@ -1,5 +1,8 @@
-import task as t
-import event as e
+from task import task
+from event import event
+from task import Priority
+from task import TaskType
+from datetime import datetime
 from enum import Enum
 
 class SleepingHabits(Enum):
@@ -25,12 +28,26 @@ class Schedule:
         
         self.myEvent.append(newEvent)
         
-    def sortTask(self,myTasks):
-        
-        return
+    def sortTask(self):
+        remainder = (x.deadline-datetime.now()).hour*4
+        sorted_tasks = sorted(self.myTasks, key=lambda x: x.priority.value + remainder)
+
+        return sorted_tasks
         
     def updateTasks(self):
         return
+    
+    def __str__(self):
+        for something in self.myTasks:
+            print(something)
         
 if __name__ == "__main__":
-    print("hello")
+    
+    t1=task(60,Priority.very_high,TaskType.school,datetime(2023,12,20,14,30))
+    t2=task(60,Priority.low,TaskType.school,datetime(2023,12,23,19,30))
+    t3=task(30,Priority.very_high,TaskType.school,datetime(2023,12,16,14,30))
+    t4=task(120,Priority.neutral,TaskType.school,datetime(2023,12,19,19,30))
+    
+    mySchedule=Schedule([t1,t2,t3,t3],[])
+    mySchedule.sortTask()
+    print(mySchedule)
