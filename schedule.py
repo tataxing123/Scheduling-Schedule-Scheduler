38d5b093e10e9e85012777594cc26363e64069a2
rency0 <rency.99.w@gmail.com>
2023-11-18 12:52:57 -0500

    lambda sorts

--- schedule.py
+++ schedule.py
@@ -10,15 +10,19 @@ class SleepingHabits(Enum):
     morning = 1
     night = 2
     both = 3
-    
+
+product_func = lambda x: x.priority.value *  x.time_remaing_to_deadline()
+sum_func = lambda x, rate: x.priority.value + rate * x.time_remaing_to_deadline()
+
 
 class Schedule:
     
-    def __init__(self,myTasks=[],myEvents=[],habits=SleepingHabits.both):
+    def __init__(self,myTasks=[],myEvents=[],habits=SleepingHabits.both, funct_to_use=product_func):
         
         self.myTasks=myTasks
         self.myEvents=myEvents
         self.habits=habits
+        self.funct_to_use = funct_to_use
     
     def addTask(self,newTask):
         
@@ -30,9 +34,7 @@ class Schedule:
         
     def sortTask(self):
         
-        sorted_tasks = sorted(self.myTasks, key=lambda x:
-            x.priority + (datetime.now()-x.deadline).total_seconds() / 900
-            )
+        sorted_tasks = sorted(self.myTasks, key= self.func_to_use) 
 
         return sorted_tasks
         
@@ -44,13 +46,15 @@ class Schedule:
             print(something)
         
 if __name__ == "__main__":
-    
+
     t1=task("t1",60,datetime(2023,11,18,22,00),Priority.low,TaskType.school)
     t2=task("t2",60,datetime(2023,11,18,22,00),Priority.very_high,TaskType.school)
     t3=task("t3",60,datetime(2023,11,19,2,00),Priority.very_high,TaskType.school)
     # t4=task(120,Priority.neutral,TaskType.school,datetime(2023,11,18,19,30))
     # title,duration,deadline,priority,type,description
-    
+    print(t1.priority.value)
+
+
     mySchedule=Schedule([t1,t2,t3],[])
     mySchedule.sortTask()
     for something in mySchedule.myTasks:
